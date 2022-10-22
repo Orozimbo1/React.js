3bb70f4aa3f0972023a73f5964882267227093c4
Matheus Orozimbo <102329940+Orozimbo1@users.noreply.github.com>
2022-10-21 23:25:23 -0300

    editando tarefas

--- my-app/src/components/main.js
+++ my-app/src/components/main.js
@@ -10,12 +10,13 @@ import { FaEdit, FaWindowClose } from "react-icons/fa"
 export default class Main extends Component {
   state = {
     novaTarefa: '',
-    tarefas: []
+    tarefas: [],
+    index: -1
   }
 
   handleSubmit = (e) => {
     e.preventDefault()
-    const { tarefas } = this.state
+    const { tarefas, index } = this.state
     let { novaTarefa } = this.state
     novaTarefa = novaTarefa.trim()
 
@@ -27,12 +28,25 @@ export default class Main extends Component {
       return
     }
 
+    if(novaTarefa === '') return
+
     const novasTarefas = [... tarefas]
 
-    this.setState({
-      tarefas: [... novasTarefas, novaTarefa],
-      novaTarefa: ''
-    })
+    if(index === -1){
+      this.setState({
+        tarefas: [... novasTarefas, novaTarefa],
+        novaTarefa: ''
+      })
+    }else{
+      novasTarefas[index] = novaTarefa
+
+      this.setState({
+        tarefas: [... novasTarefas],
+        index: -1,
+        novaTarefa: ''
+      })
+    }
+
   }
 
   handleChange = (e) => {
@@ -43,7 +57,12 @@ export default class Main extends Component {
   }
 
   handleEdit = (e, index) => {
-    console.log('Edit', index)
+    const { tarefas } = this.state
+
+    this.setState({
+      index,
+      novaTarefa: tarefas[index]
+    })
   }
 
   handleDelete = (e, index) => {
